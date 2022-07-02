d5d54c5ebd86bf332ffd34c454c56e6e3f00d179
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2022-07-02 14:16:36 +0100

    add AppEngine functional interface

---
+++ core/src/app_engine.rs
@@ -0,0 +1,25 @@
+pub struct AppEngine {
+
+
+
+}
+
+
+impl AppEngine {
+
+    pub fn new() -> Self {
+
+        AppEngine { }
+
+    }
+
+    pub fn save_configuration(&self) {
+
+    }
+
+    pub fn load_configuration(&self) {
+
+    }
+
+}
+
