e183746f9fc3cea879d2b780d8247129ea485acc
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2022-07-02 16:04:12 +0100

    seperate out persistence store interface

--- core/src/persistence.rs
+++ core/src/persistence.rs
@@ -1,18 +1,33 @@
 use crate::errors::EngineError;
 
+#[cfg(test)]
+use mockall::{automock, mock, predicate::*};
 
-pub trait PermanentStore<DataType, PrimaryKeyType> {
+#[cfg_attr(test, automock)]
+pub trait AddToStore<DataType, PrimaryKeyType> {
 
-    fn create(&self, data: DataType ) -> Result<DataType, Box<dyn EngineError>>;
+    fn update(&self, update: DataType ) -> Result<DataType, Box<dyn EngineError>>;    
 
+}
+
+#[cfg_attr(test, automock)]
+pub trait UpdateInStore<DataType, PrimaryKeyType> {
+
+    fn update(&self, update: DataType ) -> Result<DataType, Box<dyn EngineError>>;    
+
+}
+
+#[cfg_attr(test, automock)]
+pub trait RetrieveFromStore<DataType, PrimaryKeyType> {
 
     fn retrieve(&self, id: PrimaryKeyType ) -> Result<Option<DataType>, Box<dyn EngineError>>;
-
-
-    fn update(&self, update: DataType ) -> Result<DataType, Box<dyn EngineError>>;
-
     
-    fn delete(&self, update: &DataType ) -> Result<bool, Box<dyn EngineError>>;
+}
+
+#[cfg_attr(test, automock)]
+pub trait RemoveFromStore<PrimaryKeyType> {
+    
+    fn delete(&self, key: &PrimaryKeyType ) -> Result<bool, Box<dyn EngineError>>;
 
 
 }
