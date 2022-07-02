47c7163b1904ed89f6d5b2e794d047031792e05c
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2022-07-02 17:04:13 +0100

    add store manager operator setters

--- core/src/store_manager.rs
+++ core/src/store_manager.rs
@@ -3,13 +3,14 @@ use std::ops::Add;
 use super::persistence::{AddToStore, UpdateInStore, RetrieveFromStore, RemoveFromStore};
 use super::errors;
 
+
 pub struct StoreManger<DataType, PrimaryKeyType> {
 
     add: Option<Box<dyn AddToStore<DataType, PrimaryKeyType>>>,
 
     retrieve: Option<Box<dyn RetrieveFromStore<DataType, PrimaryKeyType>>>,
 
-    delete: Option<Box<dyn RemoveFromStore<DataType, PrimaryKeyType>>>,
+    delete: Option<Box<dyn RemoveFromStore<PrimaryKeyType>>>,
 
     update: Option<Box<dyn UpdateInStore<DataType, PrimaryKeyType>>>,
 
@@ -17,15 +18,41 @@ pub struct StoreManger<DataType, PrimaryKeyType> {
 
 impl <DataType, PrimaryKeyType> StoreManger<DataType, PrimaryKeyType> {
     
-    fn new() -> Self {
+    pub  fn new() -> Self {
 
         StoreManger { add: None, retrieve: None, delete: None, update: None }
 
     }
 
+    pub fn set_add_operator(&mut self, operator: Box<dyn AddToStore<DataType, PrimaryKeyType>>) {
+
+        self.add = Some(operator);
+
+    }
+
+    
+    pub fn set_retrieve_operator(&mut self, operator: Box<dyn RetrieveFromStore<DataType, PrimaryKeyType>>) {
+
+        self.retrieve = Some(operator);
+
+    }
+
+    pub fn set_update_operator(&mut self, operator: Box<dyn UpdateInStore<DataType, PrimaryKeyType>>) {
+
+        self.update = Some(operator);
+
+    }
+
+    pub fn set_delete_operator(&mut self, operator: Box<dyn RemoveFromStore<PrimaryKeyType>>) {
+
+        self.delete = Some(operator);
+
+    }
+    
+
 }
 
-impl  <DataType, PrimaryKeyType> AddToStore for StoreManger<DataType, PrimaryKeyType> {
+impl  <DataType, PrimaryKeyType> AddToStore<DataType, PrimaryKeyType> for StoreManger<DataType, PrimaryKeyType> {
     
     fn add(&self,update:DataType) -> Result<DataType, Box<dyn errors::EngineError>> {
         
@@ -47,8 +74,7 @@ impl  <DataType, PrimaryKeyType> AddToStore for StoreManger<DataType, PrimaryKey
 
 }
 
-
-impl  <DataType, PrimaryKeyType> UpdateInStore for StoreManger<DataType, PrimaryKeyType> {
+impl  <DataType, PrimaryKeyType> UpdateInStore<DataType, PrimaryKeyType> for StoreManger<DataType, PrimaryKeyType> {
     
     fn update(&self,update:DataType) -> Result<DataType, Box<dyn errors::EngineError>> {
         
@@ -71,7 +97,7 @@ impl  <DataType, PrimaryKeyType> UpdateInStore for StoreManger<DataType, Primary
 }
 
 
-impl  <DataType, PrimaryKeyType> RetrieveFromStore for StoreManger<DataType, PrimaryKeyType> {
+impl  <DataType, PrimaryKeyType> RetrieveFromStore<DataType, PrimaryKeyType> for StoreManger<DataType, PrimaryKeyType> {
 
     fn retrieve(&self,id:PrimaryKeyType) -> Result<Option<DataType>,Box<dyn errors::EngineError>> {
         
@@ -93,7 +119,7 @@ impl  <DataType, PrimaryKeyType> RetrieveFromStore for StoreManger<DataType, Pri
 
 }
 
-impl  <DataType, PrimaryKeyType> RemoveFromStore for StoreManger<DataType, PrimaryKeyType> {
+impl  <DataType, PrimaryKeyType> RemoveFromStore<PrimaryKeyType> for StoreManger<DataType, PrimaryKeyType> {
     
 
     fn delete(&self,key: &PrimaryKeyType) -> Result<bool,Box<dyn errors::EngineError>> {
