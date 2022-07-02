b0e9acfbe9ceb22c3cd16f58dcedaf7f95674d9d
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2022-07-02 14:10:42 +0100

    add project information structure

---
+++ core/src/project.rs
@@ -0,0 +1,90 @@
+use uuid::Uuid;
+
+use crate::source_control::SourceControlCFG;
+
+#[derive(Debug)]
+pub struct Project {
+
+    name: String,
+
+    source_control: Option<Box<dyn SourceControlCFG>>,
+
+    pipeline: Vec<String>,
+
+    id: String
+
+}
+
+
+impl Project {
+    
+    pub fn new(name: String ) -> Self {
+
+        Project { 
+            name, 
+            source_control: None, 
+            pipeline: Vec::new(), 
+            id: Uuid::new_v4().to_string()
+        }
+
+    }
+
+    pub fn get_name(&self) -> String {
+
+        self.name.clone()
+
+    }
+
+    pub fn set_name(mut self, name: String) -> Self {
+
+        self.name = name;
+
+        self
+
+    }
+
+    pub fn set_source_control(mut self, source_control_cfg: Option<Box<dyn SourceControlCFG>>) -> Self {
+
+        self.source_control = source_control_cfg;
+
+        return self;
+
+    }
+
+    pub fn get_source_control(&self) -> Option<&Box<dyn SourceControlCFG>> {
+
+        return self.source_control.as_ref();
+
+    }
+
+    pub fn set_id(mut self, uuid: String) -> Self {
+
+        self.id = uuid;
+
+        return self;
+
+    }
+
+    pub fn get_id(&self) -> String {
+
+        return self.id.clone();
+
+    }
+
+    
+    pub fn set_pipelines(mut self, pipelines: Vec<String>) -> Self {
+
+        self.pipeline = pipelines;
+
+        return self;
+        
+    }
+
+    pub fn get_pipelines(&self) -> &Vec<String> {
+
+        return &self.pipeline;
+
+    }
+
+}
+
