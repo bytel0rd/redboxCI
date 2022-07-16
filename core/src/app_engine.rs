c9e6f14698984a66f510d8c0e017070f8e31c294
Oyegoke Abiodun <abiodunogundijo@gmail.com>
2022-07-16 08:18:06 +0100

    add AppEngine top level interface

--- core/src/app_engine.rs
+++ core/src/app_engine.rs
@@ -1,25 +1,31 @@
-pub struct AppEngine {
+use super::workspace_manager::WorkspaceManager;
 
+use super::project_manager::ProjectManager;
 
+pub struct AppEngine<'pm, 'ws> {
 
-}
+    project_manager: &'pm ProjectManager<'pm>,
 
-
-impl AppEngine {
-
-    pub fn new() -> Self {
-
-        AppEngine { }
-
-    }
-
-    pub fn save_configuration(&self) {
-
-    }
-
-    pub fn load_configuration(&self) {
-
-    }
+    workspace_manager: &'ws WorkspaceManager<'ws>
 
 }
+
+
+// impl AppEngine {
+
+//     // pub fn new() -> Self {
+
+//     //     AppEngine { }
+
+//     // }
+
+//     pub fn save_configuration(&self) {
+
+//     }
+
+//     pub fn load_configuration(&self) {
+
+//     }
+
+// }
 
